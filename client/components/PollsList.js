b379717d5c8287c88ad06ecc48e65890320395df
yonax <crazy.yonax@gmail.com>
2016-09-08 15:18:42 +0500

    little cleanup

--- client/components/PollsList.js
+++ client/components/PollsList.js
@@ -4,12 +4,12 @@ import { Link } from 'react-router';
 const PollsList = ({ polls }) => (
   <div className="poll-list">
     { polls.map(poll => 
-      <li key={poll._id} className="poll well">
+      <div key={poll._id} className="poll well">
         <Link to={`/polls/${poll._id}`}>
           <h4>{poll.topic}</h4>
         </Link>
         <span>Votes: {poll.choices.reduce((acc, choice) => acc + choice.votes, 0)}</span>
-      </li>
+      </div>
     )}
   </div>
 )
